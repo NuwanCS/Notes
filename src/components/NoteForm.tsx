82489ddbc67ff409df052ffa19a37854ef0c4d0e
Nuwan Senevirathne <nuwan@neveroffside.com>
2023-12-27 21:18:51 +0530

    added delete funtionality to option list

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,23 +1,17 @@
-import { Form } from "react-bootstrap";
-import { MultiSelectDropdown } from "./utils";
+import { Form } from 'react-bootstrap';
+import { MultiSelectDropdown } from './utils';
 
-export function NoteForm () {
-
-    return (
-        <Form>
-            <Form.Group>
-                <Form.Label>
-                    Title
-                </Form.Label>
-                <Form.Control required/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>
-                    Tags
-                </Form.Label>
-                <MultiSelectDropdown/>
-            </Form.Group>
-        </Form>
-
-    )
+export function NoteForm() {
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control required />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Tags</Form.Label>
+        <MultiSelectDropdown />
+      </Form.Group>
+    </Form>
+  );
 }
--- src/components/utils/MulitiSelectDropDown.tsx
+++ src/components/utils/MulitiSelectDropDown.tsx
@@ -7,54 +7,89 @@ interface Option {
 }
 
 export const MultiSelectDropdown: React.FC = () => {
+  const [showDropdownList, setShowDropDownList] = useState(false);
+  const [selectedItemsList, setSelectedItemsList] = useState<Option[]>([]);
+  const options: Option[] = [
+    {
+      id: 1,
+      label: 'one',
+    },
+    {
+      id: 2,
+      label: 'two',
+    },
+  ];
 
-    const [showDropdownList, setShowDropDownList] = useState(false);
-    const [selectedItemsList, setSelectedItemsList] = useState<Option[]>([]);
-    const options: Option[] = [
-        {
-            id: 1,
-            label: 'one'
-        },
-        {
-            id: 2,
-            label: 'two'
-        }
-    ];
+  const [renderedOptions, setRenderedOptions] = useState<Option[]>(options);
+  const hanldeDropDownClick = () => {
+    setShowDropDownList(!showDropdownList);
+  };
 
-    const [renderedOptions, setRenderedOptions] = useState<Option[]>(options);
-    const hanldeDropDownClick = () => {
-        setShowDropDownList(!showDropdownList)
-    }
+  const handleOptionSelected = (optionLabel: Option) => {
+    setSelectedItemsList((prev) => [...prev, optionLabel]);
+    setRenderedOptions(
+      renderedOptions.filter((opt) => opt.id !== optionLabel.id)
+    );
+  };
 
-    const handleOptionSelected = (optionLabel: Option) => {
-        setSelectedItemsList((prev)=> [...prev, optionLabel]);
-        setRenderedOptions(renderedOptions.filter((opt) => opt.id !== optionLabel.id))
-    }
+  const hanldeSelectedOptionClose = (optionLabel: Option) => {
+    setSelectedItemsList(
+      selectedItemsList.filter((opt) => opt.id !== optionLabel.id)
+    );
+    setRenderedOptions((prev) => [...prev, optionLabel]);
+  };
 
-    const hanldeSelectedOptionClose = (optionLabel: Option) => {
-        setSelectedItemsList(selectedItemsList.filter((opt) => opt.id !== optionLabel.id));
-        setRenderedOptions((prev)=> [...prev, optionLabel])
-    }
+  const hanldeOptionListItemClose = (optionLabel: Option) => {
+    // Need to add a condition to stop delte if the tag is being used somewhere
+    // const foundTags =
+    const results = options.filter((opt) => opt.id !== optionLabel.id);
+    setRenderedOptions(results);
+  };
   return (
-    <div className='h-10 w-100'>
-        <div onClick={hanldeDropDownClick} className="selecte-list h-100 w-100 d-flex border" style={{ minWidth: '100%', minHeight: '40px' }}>
-            {selectedItemsList.length> 0 && selectedItemsList.map(item => {
-                return(
-                <div className='d-flex p-1 border border-primary m-1 rounded' key={`selected-${item.id}`}>
-                    {item.label}
-                    <CloseIcon className='sx' onClick={() => hanldeSelectedOptionClose(item)} />
+    <div className="h-10 w-100">
+      <div
+        onClick={hanldeDropDownClick}
+        className="selecte-list h-100 w-100 d-flex border"
+        style={{ minWidth: '100%', minHeight: '40px' }}
+      >
+        {selectedItemsList.length > 0 &&
+          selectedItemsList.map((item) => {
+            return (
+              <div
+                className="d-flex p-1 border border-primary m-1 rounded"
+                key={`selected-${item.id}`}
+              >
+                {item.label}
+                <CloseIcon
+                  className="sx"
+                  onClick={() => hanldeSelectedOptionClose(item)}
+                />
+              </div>
+            );
+          })}
+      </div>
+      <div>
+        <div>
+          {showDropdownList &&
+            renderedOptions.map((op) => {
+              return (
+                <div className="d-flex">
+                  <div
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => handleOptionSelected(op)}
+                    key={op.id}
+                  >
+                    {op.label}{' '}
+                  </div>
+                  <CloseIcon
+                    className="sx"
+                    onClick={() => hanldeOptionListItemClose(op)}
+                  />
                 </div>
-            )})}
-        </div>
-        <div  >
-            <div>
-            {showDropdownList && renderedOptions.map(op => {
-                return (<div style={{cursor: 'pointer'}} onClick={() => handleOptionSelected(op)} key={op.id}>{op.label}</div>)
+              );
             })}
-            </div>
         </div>
+      </div>
     </div>
-  )
+  );
 };
-
-
